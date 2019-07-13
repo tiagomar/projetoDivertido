5371c0e983055d6eae30f4d2c18883554220136c
TiagoMarques <tiagomarques@MacBook-Pro-5.local>
2019-07-13 15:11:50 -0300

    Atualizando page object

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -10,4 +11,15 @@ public class HomePage {
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
+
+    //Elements Locators
+    By enderecoOuCepInputText = By.cssSelector(".contentform input[name='relaxation']");
+
+    //Actions
+    public HomePage insertEnderecoOuCep(String enderecoOuCep) {
+        driver.findElement(enderecoOuCepInputText).sendKeys(enderecoOuCep);
+        return this;
+    }
+
+
 }
